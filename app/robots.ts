8ebd5d8f2f264d00bcbc03b7586ae6e82e8e131b
Cody <cody@dreamind.cz>
2026-04-22 09:46:11 +0000

    Refine robots rules for AI search visibility

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,20 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+    ],
     sitemap: "https://cody.dreamind.cz/sitemap.xml",
   };
 }
